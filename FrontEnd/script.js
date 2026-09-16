81a852a2a21015959d62ad79d67e1eb77fddd418
Matrasit RATCHAPHAKDEE <r.matrasit@gmail.com>
2026-09-16 15:54:34 +0200

    Finalisation du projet et gestion des erreurs

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -30,19 +30,15 @@ function displayWorks(works) {
         caption.textContent = works[i].title;   
         figure.dataset.id = works[i].id;             
 
-        
-        
         // Ajoute l'image et sa légende à l'intérieur de la figure                    
         
         figure.appendChild(image); 
         figure.appendChild(caption);                       
-                 // Ajoute la figure à la galerie
+         // Ajoute la figure à la galerie
         gallery.appendChild(figure);            
     }  
-    
 }
 function displayModalWorks(works) {   
-
     // Sélectionne dans le DOM l'élément HTML qui possède la classe "gallery"
     const gallery = document.querySelector(".modal-gallery");
     gallery.innerHTML = "";                                             // Clear the gallery before displaying filtered works
@@ -59,36 +55,228 @@ function displayModalWorks(works) {
         image.alt = works[i].title;      
 
         buttonTrash.classList.add("delete-work");
-        iconTrash.classList.add("fa-regular", "fa-trash-can");
-
-                          
-                            
-        figure.appendChild(image);                                      // Ajoute l'image et sa légende à l'intérieur de la figure
-        gallery.appendChild(figure);                                    // Ajoute la figure à la galerie
+        iconTrash.classList.add("fa-regular", "fa-trash-can");     
+        figure.appendChild(image);                                      
+        gallery.appendChild(figure);                                    
         figure.appendChild(buttonTrash);
         buttonTrash.appendChild(iconTrash); 
         
         buttonTrash.addEventListener("click", async function(event){
             const id = works[i].id;
-            console.log("Clic sur la poubelle, id :", id);
             const token = sessionStorage.getItem("token");
-            const response = await fetch('http://localhost:5678/api/works/${id}`',{
+            const response = await fetch(`http://localhost:5678/api/works/${id}`,{
                 method: "DELETE",
                 headers: {
                     "Authorization": `Bearer ${token}`
- 
                 }
             });
-            console.log("status :", response.status);
-            console.log("ok :", response.ok);
             if(response.ok){
                 figure.remove();
                 const mainFigure = document.querySelector(`figure[data-id="${id}"]`);
                 mainFigure.remove();
+                const index = works.findIndex(function(work) {
+                 return work.id === id;
+                });
+                
+                if (index !== -1) {
+                    works.splice(index, 1);
+                }
+            };
+        });
+    }; 
+}
+function openAddPhotoPage(categories, works){
+    const buttonAddPhoto = document.querySelector(".buttonAddPhoto");
+    buttonAddPhoto.addEventListener("click", function(){
+        const modalTitre = document.querySelector(".modal-titre");
+        modalTitre.textContent = "Ajout photo";
+
+        const modalGallery = document.querySelector(".modal-gallery");
+        modalGallery.style.display = "none";
+        buttonAddPhoto.style.display = "none";
+
+        const buttonBack = document.createElement("button");
+        buttonBack.classList.add("buttonBack");
+        const iconBack = document.createElement("i");
+        iconBack.classList.add("fa-solid", "fa-arrow-left");
+        buttonBack.appendChild(iconBack);
+
+        const modalWrapper = document.querySelector(".modal-wrapper");
+        const buttonCloseModal = document.querySelector(".ButtonCloseModal");
+        modalWrapper.insertBefore(buttonBack,buttonCloseModal);
+
+        function backToGallery() {
+            buttonBack.remove();
+            modalTitre.textContent = "Gallery Photo";
+            modalGallery.style.display = "grid";
+            buttonAddPhoto.style.display = "block";
+            addPhotoForm.remove();
+        }
+        buttonBack.addEventListener("click",function(){
+            backToGallery();
+        });
+        
+        const addPhotoForm = document.createElement("form");
+        addPhotoForm.classList.add("add-photo-form");
+        modalWrapper.appendChild(addPhotoForm);
+
+        const uploadZone = document.createElement("div");
+        uploadZone.classList.add("upload-zone");
+        addPhotoForm.appendChild(uploadZone);
+
+        const iconImag = document.createElement("i");
+        iconImag.classList.add("fa-solid", "fa-image");
+        uploadZone.appendChild(iconImag);
+
+        const errorImage =document.createElement("p");
+        errorImage.classList.add("error-image")
+        uploadZone.appendChild(errorImage);
+        
+
+        const inputImage = document.createElement("input");
+        inputImage.type = "file";
+        inputImage.id = "image";
+        inputImage.name = "image";
+        inputImage.accept = "image/png, image/jpeg";
+        uploadZone.appendChild(inputImage);
+
+        inputImage.addEventListener("change",function(){
+            const imageFile = inputImage.files[0];
+            
+           if (imageFile) {
+                errorImage.textContent = "";
+                 if (imageFile.size > 4 * 1024 * 1024) {
+                    inputImage.value = "";
+                    errorImage.textContent = "L'image ne doit pas dépasser 4 Mo."
+                    return;
+                }
+
+            const imageUrl = URL.createObjectURL(imageFile); //réer une URL temporaire permettant au navigateur d'afficher le fichier sélectionné
+            
+            const previewImage = document.createElement("img");
+            previewImage.src = imageUrl;
+            previewImage.alt = "Prévisualisation de l'image";
+            previewImage.classList.add("preview-image");
+
+            iconImag.style.display = "none";
+            labelAddPhoto.style.display = "none";
+            textFormat.style.display = "none";
+
+            uploadZone.appendChild(previewImage);
             }
         });
-    }  
-    
+
+        const labelAddPhoto = document.createElement("label");
+        labelAddPhoto.classList.add("button-add-photo-form");
+        labelAddPhoto.htmlFor = "image";
+        labelAddPhoto.textContent = "+ Ajouter photo";
+        uploadZone.appendChild(labelAddPhoto);
+
+        const textFormat = document.createElement("p");
+        textFormat.classList.add("text-format");
+        textFormat.textContent = "jpg, png : 4mo max"
+        uploadZone.appendChild(textFormat);
+
+        const formGroupTitre = document.createElement("div");
+        formGroupTitre.classList.add("form-group");
+        addPhotoForm.appendChild(formGroupTitre);
+        const formGroupCategorie = document.createElement("div");
+        formGroupCategorie.classList.add("form-group");
+        addPhotoForm.appendChild(formGroupCategorie);
+
+        const labelTitre = document.createElement("label");
+        labelTitre.textContent ="Title";
+        labelTitre.htmlFor = "titre";
+        formGroupTitre.appendChild(labelTitre);
+
+        const inputTitre = document.createElement("input");
+        inputTitre.id = "titre";
+        inputTitre.type = "text";
+        inputTitre.name = "titre";
+        formGroupTitre.appendChild(inputTitre);
+
+        const labelCategorie = document.createElement("label");
+        labelCategorie.textContent ="Catégorie";
+        labelCategorie.htmlFor = "categorie";
+        formGroupCategorie.appendChild(labelCategorie);
+
+        const selectCategorie = document.createElement("select");
+        selectCategorie.id = "categorie";
+        selectCategorie.name = "categorie";        
+        formGroupCategorie.appendChild(selectCategorie);
+
+        const optionVide = document.createElement("option");
+        optionVide.value = "";
+        optionVide.textContent = "";
+        selectCategorie.appendChild(optionVide);
+
+        categories.forEach(function(categorie){
+            
+                const optionCategorie = document.createElement("option");
+                optionCategorie.textContent = categorie.name;
+                optionCategorie.value = categorie.id;
+                selectCategorie.appendChild(optionCategorie);
+            }   
+        );
+
+        const buttonValidate = document.createElement("button");            //Création bouton "valider"
+        buttonValidate.classList.add("button-validate");
+        buttonValidate.textContent = "Valider";
+        buttonValidate.type = "submit";
+        buttonValidate.disabled = true;
+        addPhotoForm.appendChild(buttonValidate);
+
+        const errorSubmit = document.createElement("p");
+        errorSubmit.classList.add("error-submit");
+        addPhotoForm.appendChild(errorSubmit);
+
+        function checkFormValidity() {
+        const imageIsValid = inputImage.files.length > 0;
+        const titleIsValid = inputTitre.value.trim() !== "";
+        const categoryIsValid = selectCategorie.value !== "";
+        if (imageIsValid && titleIsValid && categoryIsValid) {
+            buttonValidate.disabled = false;
+            buttonValidate.classList.add("active");
+        } else {
+            buttonValidate.disabled = true;
+            buttonValidate.classList.remove("active");
+        }
+        }
+        inputImage.addEventListener("change", checkFormValidity);
+        inputTitre.addEventListener("input", checkFormValidity);
+        selectCategorie.addEventListener("change", checkFormValidity);
+
+        addPhotoForm.addEventListener("submit", async function(event) {
+            event.preventDefault();
+            const formData = new FormData();
+
+            formData.append("image", inputImage.files[0]);
+            formData.append("title", inputTitre.value);
+            formData.append("category", selectCategorie.value);
+
+            for (const [key, value] of formData.entries()) {
+            }
+
+            const token = sessionStorage.getItem("token");
+            const response = await fetch("http://localhost:5678/api/works", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                body: formData
+            
+            });
+            if (response.ok) {
+                    const newWork = await response.json();
+                    works.push(newWork);
+                    displayWorks(works);
+                    displayModalWorks(works);
+                    backToGallery();
+                } else{
+                    errorSubmit.textContent = "Une erreur est survenue lors de l'ajout du projet"
+                } 
+        });  
+    });
 }
 function createFilters(categories) {
     const portfolio = document.querySelector("#portfolio");
@@ -160,7 +348,7 @@ function login() {
             })
             const data = await response.json();                         //conversion de la réponse JSON en objet JavaScript
 
-            if (response.status === 200) {                              // si la connexion est réussie (code de statut 200)
+            if (response.ok) {                              // si la connexion est réussie (code de statut 200)
                 sessionStorage.setItem("token", data.token);            //Stockage du token dans sessionStorage
                 window.location.href = "index.html";                    // Redirection vers la page index.html après connexion réussie
             } else {                                                    //gestion message d'erreur si l'identifiant ou le mot de passe est incorrect
@@ -181,7 +369,7 @@ function login() {
     });
 }
 function editPage(){
-    const loginLink = document.querySelector(".login-link"); // Sélectionne le lien de connexion dans le DOM
+    const loginLink = document.querySelector(".login-link"); 
             loginLink.textContent = "logout";                       // Change le texte du lien de connexion en "logout"
             loginLink.addEventListener("click", function(event) {   // Ajoute un événement de clic au lien de connexion
                 event.preventDefault();                              // Empêche le comportement par défaut du lien (redirection)
@@ -189,15 +377,15 @@ function editPage(){
                 window.location.href = "index.html";                // Redirige vers la page index.html après la déconnexion
             });
             const editMode = document.createElement("div");        // Crée un nouvel élément div pour le mode édition
-            editMode.classList.add("edit-mode");                   // Ajoute la classe "edit-mode" à l'élément
+            editMode.classList.add("edit-mode");                   
             editMode.textContent = "Mode édition";                 // Définit le texte de l'élément en "Mode édition"
 
-            const body = document.querySelector("body");           // Sélectionne l'élément body dans le DOM
+            const body = document.querySelector("body");           
             const header = document.querySelector("header");        
             body.insertBefore(editMode, header);                    // Insère l'élément editMode avant l'élément header dans le body
 
-            const modifier =document.createElement("button");       // Crée un nouveau balise button
-            modifier.classList.add("modif-picture");                // ajuster class au balise
+            const modifier =document.createElement("button");       
+            modifier.classList.add("modif-picture");                
             modifier.textContent = "Modifier";
 
             const portfolioTitle = document.querySelector(".portfolio-title");  
@@ -205,7 +393,7 @@ function editPage(){
             // Crée l'icon pour le text modifier 
             const iconModif = document.createElement("i");                  // Crée la balise "i" pour l'icon
             const buttonModif = document.querySelector(".modif-picture");   
-            iconModif.classList.add("fa-regular", "fa-pen-to-square");      // ajoute la classe à l'icone  
+            iconModif.classList.add("fa-regular", "fa-pen-to-square");        
             buttonModif.insertBefore(iconModif,buttonModif.firstChild);     // Insert icon avant premier l'élément dans le conteneur bouton
             
             // Crée l'icon pour le mode édition en haur de la page
@@ -229,7 +417,7 @@ function editPage(){
 function openModalPage(buttonModif){
         const openModal = document.querySelector("#modal");
         openModal.style.display = null;                             //retire le display: none inline, donc CSS .modal1 { display: flex; } reprend la main.
-        openModal.removeAttribute("aria-hidden");                   //la modale n’est plus déclarée comme cachée pour les technologies d’assistance.
+        openModal.removeAttribute("aria-hidden");                   
         openModal.setAttribute("aria-modal", "true");  
                      //indique que cette boîte de dialogue est maintenant active comme modale.
         const closeModalOnClick = function(event){
@@ -270,7 +458,7 @@ async function init() {
         displayModalWorks(works) 
         if (token !== null) {                                       // Vérifie si le token existe dans sessionStorage, ce qui signifie que l'utilisateur est connecté
             editPage();
-            
+            openAddPhotoPage(categories,works);
 
         } else {                                                    // Si le token n'existe pas, l'utilisateur n'est pas connecté, on crée les filtres pour les travaux
             createFilters(categories);
